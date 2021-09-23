190a4248d4e3aa4ce13541130ab0e6924f734edd
LuuChiara <cipbelu16@gmail.com>
2021-09-23 17:09:46 -0300

    firebase-crud

--- firebase-ej1/main.js
+++ firebase-ej1/main.js
@@ -3,8 +3,8 @@ const db= firebase.firestore()
 
 const guardarcliente = async () =>{
 
-    const apellido= document.getElementById("inp-nom").value
-    const nombre= document.getElementById("inp-ape").value
+    const nombre= document.getElementById("inp-nom").value
+    const apellido= document.getElementById("inp-ape").value
     const dni= document.getElementById("inp-dni").value
 
     const uncliente={
@@ -18,6 +18,44 @@ const guardarcliente = async () =>{
     document.getElementById("inp-ape").value='';
     document.getElementById("inp-nom").value='';
     document.getElementById("inp-dni").value='';
+    listar_clientes();
 }
 const boton= document.getElementById("btn-obtener")
 boton.addEventListener("click", guardarcliente);
+
+
+function eliminar(id) {
+    db.collection("Clientes").doc(id).delete()
+    listar_clientes();
+
+}
+
+const listar_clientes = async () =>{
+
+    const clientes= await db.collection("Clientes").get()
+    let lista_clientes=[]
+
+    const lista= clientes.docs.map(doc=> {
+        lista_clientes=doc.data()
+        lista_clientes.id=doc.id;
+        return lista_clientes;
+    });
+
+    let totalfilas=[]
+    lista.forEach(element => {
+        let fila=`
+            <tr class="table-success">
+                <td>${element.nombre}</td>
+                <td>${element.apellido}</td>
+                <td>${element.dni}</td>
+                <td>
+                    <button onclick="eliminar('${element.id}')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                </td>
+            </tr>
+        `
+        totalfilas.push(fila)
+    });
+    document.getElementById("tbody").innerHTML=totalfilas.join('')
+}
+
+listar_clientes()
