58ba8202e42ec9de5fb6b20e0100bc36801a0e75
LuuChiara <cipbelu16@gmail.com>
2021-09-30 17:06:03 -0300

    crud updated -actualizar, vaciar y editar

--- firebase-ej1/main.js
+++ firebase-ej1/main.js
@@ -15,16 +15,15 @@ const guardarcliente = async () =>{
 
     await db.collection("Clientes").doc().set(uncliente)
 
-    document.getElementById("inp-ape").value='';
-    document.getElementById("inp-nom").value='';
-    document.getElementById("inp-dni").value='';
     listar_clientes();
+    vaciar();
 }
 const boton= document.getElementById("btn-obtener")
 boton.addEventListener("click", guardarcliente);
 
 
-function eliminar(id) {
+function eliminar() {
+    const id=document.getElementById("inp-id").value
     db.collection("Clientes").doc(id).delete()
     listar_clientes();
 
@@ -49,7 +48,9 @@ const listar_clientes = async () =>{
                 <td>${element.apellido}</td>
                 <td>${element.dni}</td>
                 <td>
-                    <button onclick="eliminar('${element.id}')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                    <button onclick="llenar_input_oculto('${element.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                    <button onclick="editar_cliente('${element.nombre}', '${element.apellido}', ${element.dni}, '${element.id}')" class="btn btn-success btn-sm"><i class="fa fa-edit"></i></button>
+
                 </td>
             </tr>
         `
@@ -58,4 +59,39 @@ const listar_clientes = async () =>{
     document.getElementById("tbody").innerHTML=totalfilas.join('')
 }
 
-listar_clientes()
+listar_clientes();
+
+function llenar_input_oculto(id) {
+    document.getElementById("inp-id").value=id;
+}
+
+function editar_cliente(nom,ape,dni,id) {
+    document.getElementById("inp-nom").value= nom;
+    document.getElementById('inp-ape').value= ape;
+    document.getElementById('inp-dni').value= dni;
+    document.getElementById('inp-id').value=id;
+}
+
+function actualizar_cliente() {
+    const nom= document.getElementById("inp-nom").value;
+    const ape= document.getElementById('inp-ape').value;
+    const dni= document.getElementById('inp-dni').value;
+    const id= document.getElementById('inp-id').value;
+
+    const clienteActualizado={
+        nombre: nom,
+        apellido: ape,
+        dni: dni
+    }
+
+    db.collection("Clientes").doc(id).update(clienteActualizado)
+    listar_clientes();
+    vaciar();
+}
+
+function vaciar() {
+    document.getElementById("inp-nom").value= '';
+    document.getElementById('inp-ape').value= '';
+    document.getElementById('inp-dni').value= '';
+    document.getElementById('inp-id').value= '';
+}
